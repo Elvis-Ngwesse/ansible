5b8d82a956fa12e0560e09c3582bbdd4fa91b9a8
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2025-05-15 09:05:07 +0100

    remover teraform

--- terraform/modules/apache_module/outputs.tf
+++
@@ -1,9 +0,0 @@
-
-
-output "apache_ip" {
-  value = {
-    name = google_compute_instance.web_server_apache.name
-    ip   = google_compute_instance.web_server_apache.network_interface[0].access_config[0].nat_ip
-  }
-  description = "The name and public IP of the web_server_apache"
-}
